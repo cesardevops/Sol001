d1ebb5f5470c9fb03e9f44bcbe10ec92b20c585e
Cesar Freitas <33384671+cesardevops@users.noreply.github.com>
2018-07-06 22:35:55 -0500

    Actualizacion final.
    migracion de gh-pages.

---
+++ js/menu-main.js
@@ -0,0 +1,46 @@
+$(window).on('scroll' , function(){
+	if($(window).scrollTop()){
+		$('nav').addClass('small');
+	}else {
+		$('nav').removeClass('small');
+	}
+});
+
+$(function(){
+	$(".fa-times").hide();
+	$(".fa-bars").click(function(){
+		$(this).hide();
+		$(".fa-times").show();
+		$("#mob-menu").css("left","0px");
+		$(".back-menu-shadow").addClass("open-back-menu-shadow");
+	});
+
+	$(".fa-times").click(function(){
+		hideMenuMobile();
+	});
+
+	$(".back-menu-shadow").click(function(){
+		hideMenuMobile();
+	});
+
+	function hideMenuMobile()
+	{
+		$(".fa-times").hide();
+		$(".fa-bars").show();
+		$("#mob-menu").css("left","-90%");
+		$(".back-menu-shadow").removeClass("open-back-menu-shadow");
+	}
+	getWidth();
+	
+	function getWidth(){
+		var vpwidth;
+		vpwidth 	= window.innerWidth;
+		if(vpwidth >= 740)
+		{
+			hideMenuMobile();
+		}
+	}
+
+	window.addEventListener('resize',getWidth);
+
+});
